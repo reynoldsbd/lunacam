4437a25bd1d87af2f9b6d74aef4df5eb2376cb2e
Bobby Reynolds <bobby@reynoldsbd.net>
2019-05-27 00:12:43 -0700

    bulma switch, -jquery

---
+++ js/base.js
@@ -0,0 +1,13 @@
+// -------------------------------------------------------------------------------------------------
+// Bulma Navbar Burger
+// -------------------------------------------------------------------------------------------------
+
+var burgers = document.getElementsByClassName('navbar-burger');
+var menus = document.getElementsByClassName('navbar-menu');
+
+Array.from(burgers).forEach(burger => burger.onclick = toggleMenu);
+
+function toggleMenu() {
+    Array.from(burgers).forEach(burger => burger.classList.toggle('is-active'));
+    Array.from(menus).forEach(menu => menu.classList.toggle('is-active'));
+}
