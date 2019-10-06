29512609973c9768c26953369f600a65d5c5d515
Bobby Reynolds <bobby@helicon.flop>
2019-10-06 11:26:37 -0700

    fix agent system integration bugs

--- tools/pi-gen/agent/00-video/01-run.sh
+++ tools/pi-gen/agent/00-video/01-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 install -m 644 files/modules-load.conf "${ROOTFS_DIR}/usr/lib/modules-load.d/lcagent.conf"
-install -m 644 files/udev.rules        "${ROOTFS_DIR}/usr/lib/udev/rules.d"
+install -m 644 files/udev.rules        "${ROOTFS_DIR}/usr/lib/udev/rules.d/lcagent.rules"
 
 cat >> "${ROOTFS_DIR}/boot/config.txt" << EOF
 
--- tools/pi-gen/agent/02-agent/00-run.sh
+++ tools/pi-gen/agent/02-agent/00-run.sh
@@ -2,7 +2,9 @@
 
 install -m 644 files/tmpfiles.conf   "${ROOTFS_DIR}/usr/lib/tmpfiles.d/lcagent.conf"
 install -m 644 files/lcagent.service "${ROOTFS_DIR}/usr/lib/systemd/system/lcagent.service"
-install -m 644 files/lcagent         "${ROOTFS_DIR}/usr/bin/lcagent"
+
+# lcagent binary is built and placed under files by Makefile
+install -m 755 files/lcagent         "${ROOTFS_DIR}/usr/bin/lcagent"
 
 on_chroot << EOF
 systemctl enable lcagent
