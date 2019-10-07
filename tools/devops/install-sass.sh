ecb6dae46045f0de0cce080f47c90f65db79b85a
Bobby Reynolds <bobby@helicon.flop>
2019-10-06 22:09:42 -0700

    use bash to install sass

---
+++ tools/devops/install-sass.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Installs the Dart-based Sass CLI tool
+
+version=$1
+destination=$(realpath $2)
+
+echo "creating destination directory"
+mkdir -p $destination
+
+echo "downloading Sass version $version"
+sassTarball="$destination/sass.tar.gz"
+sassUrl="https://github.com/sass/dart-sass/releases/download/$version/dart-sass-$version-linux-x64.tar.gz"
+wget $sassUrl -qO $sassTarball
+
+echo "extracting"
+tar -xzf $sassTarball -C $destination
+
+echo "configuring PATH"
+echo "##vso[task.setvariable variable=PATH]$destination/dart-sass:$PATH"
