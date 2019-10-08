7669cd84dc25501103ebed8b70236841008b91db
Bobby Reynolds <bobby@helicon.flop>
2019-10-08 16:58:14 -0700

    fix portal file perms

--- tools/pi-gen/portal/01-portal/00-run.sh
+++ tools/pi-gen/portal/01-portal/00-run.sh
@@ -1,16 +1,22 @@
 #!/bin/bash -e
 
 install -m 644 files/lcportal.service "${ROOTFS_DIR}/usr/lib/systemd/system/lcportal.service"
-install -m 644 files/lcportal         "${ROOTFS_DIR}/usr/bin/lcportal"
 install -m 440 files/sudoers          "${ROOTFS_DIR}/etc/sudoers.d/lcportal"
 
-mkdir -p                  "${ROOTFS_DIR}/usr/share/lunacam/static"
-rsync -r files/static/    "${ROOTFS_DIR}/usr/share/lunacam/static"
-chmod -R 644              "${ROOTFS_DIR}/usr/share/lunacam/static"
+# lcportal binary is built and placed under files by Makefile
+install -m 755 files/lcportal         "${ROOTFS_DIR}/usr/bin/lcportal"
 
-mkdir -p                  "${ROOTFS_DIR}/usr/share/lunacam/templates"
-rsync -r files/templates/ "${ROOTFS_DIR}/usr/share/lunacam/templates"
-chmod -R 644              "${ROOTFS_DIR}/usr/share/lunacam/templates"
+function install_dir() {
+    src=$1
+    dst=$2
+    mkdir -p $dst
+    rsync -r $src $dst
+    find $dst -type d -exec chmod 755 {} +
+    find $dst -type f -exec chmod 644 {} +
+}
+
+install_dir "files/static/"    "${ROOTFS_DIR}/usr/share/lunacam/static"
+install_dir "files/templates/" "${ROOTFS_DIR}/usr/share/lunacam/templates"
 
 on_chroot << EOF
 systemctl enable lcportal
