10f3fe4c607eb1c4bbe3bd0640dfe85ec7b44061
Bobby Reynolds <bobby@helicon.flop>
2019-10-07 21:56:00 -0700

    create dirs before rsync

--- tools/pi-gen/portal/01-portal/00-run.sh
+++ tools/pi-gen/portal/01-portal/00-run.sh
@@ -4,9 +4,11 @@ install -m 644 files/lcportal.service "${ROOTFS_DIR}/usr/lib/systemd/system/lcpo
 install -m 644 files/lcportal         "${ROOTFS_DIR}/usr/bin/lcportal"
 install -m 440 files/sudoers          "${ROOTFS_DIR}/etc/sudoers.d/lcportal"
 
+mkdir -p                  "${ROOTFS_DIR}/usr/share/lunacam/static"
 rsync -r files/static/    "${ROOTFS_DIR}/usr/share/lunacam/static"
 chmod -R 644              "${ROOTFS_DIR}/usr/share/lunacam/static"
 
+mkdir -p                  "${ROOTFS_DIR}/usr/share/lunacam/templates"
 rsync -r files/templates/ "${ROOTFS_DIR}/usr/share/lunacam/templates"
 chmod -R 644              "${ROOTFS_DIR}/usr/share/lunacam/templates"
 
