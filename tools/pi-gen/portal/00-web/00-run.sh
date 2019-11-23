e3cf3b178e9b389ba588f3d1784677a1237735f8
Bobby Reynolds <bobby@reynoldsbd.net>
2019-11-23 14:09:13 -0800

    fix missing directory

--- tools/pi-gen/portal/00-web/00-run.sh
+++ tools/pi-gen/portal/00-web/00-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-install -m 644 files/static-files.conf "${ROOTFS_DIR}/usr/share/lunacam/nginx/static-files.conf"
+install -m 644 -D files/static-files.conf "${ROOTFS_DIR}/usr/share/lunacam/nginx/static-files.conf"
