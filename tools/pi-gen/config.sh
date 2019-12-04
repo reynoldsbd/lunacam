18a7445e235900babca2c9002c553c84d1716215
Bobby Reynolds <bobby@reynoldsbd.net>
2019-12-03 20:45:36 -0800

    incremental img build

--- tools/pi-gen/config.sh
+++ tools/pi-gen/config.sh
@@ -1,11 +1,14 @@
-export IMG_NAME="lunacam"
-export DEPLOY_ZIP=1
 export LOCALE_DEFAULT="en_US.UTF-8"
 export KEYBOARD_KEYMAP="us"
 export KEYBOARD_LAYOUT="English (US)"
 export TIMEZONE_DEFAULT="America/Los_Angeles"
+
 export FIRST_USER_NAME="admin"
 export FIRST_USER_PASS="admin"
 export ENABLE_SSH="1"
+
+export IMG_NAME="lunacam"
+export IMG_FILENAME="lunacam"
+export ZIP_FILENAME="lunacam"
 
 export LC_UTILS="$(pwd)/utils.sh"
